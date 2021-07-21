ce672819c721bb42bf03ee9a5830de8f54191803
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-07-21 21:35:17 +0530

    VERCEL_URL stuff

--- src/pages/p/[user_username].tsx
+++ src/pages/p/[user_username].tsx
@@ -67,7 +67,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         // ? `http://localhost:3000/api/v1/user?user_username=${user_username}`
         `${
           process.env.VERCEL_URL
-            ? process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
             : "http://localhost:3000"
         }/api/v1/user?user_username=${user_username}`
       );
