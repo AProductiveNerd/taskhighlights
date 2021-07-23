b3cdfb73afe831be7670d339d3e4d53b669ce23a
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-07-23 19:38:13 +0530

    Add the three dot menu for individual tasks

--- src/components/tasks/IndividualTask.tsx
+++ src/components/tasks/IndividualTask.tsx
@@ -1,9 +1,13 @@
 import {
   ArchiveIcon,
+  DotsCircleHorizontalIcon,
+  DotsVerticalIcon,
   EyeIcon,
   EyeOffIcon,
   TrashIcon
 } from "@heroicons/react/solid";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import {
   Useful_Todo,
   todo_archived,
@@ -22,7 +26,6 @@ import {
 } from "../../utils/onClickHelpers";
 
 import { Story } from "@prisma/client";
-import { useState } from "react";
 
 export const IndividualTask = ({
   todo: {
@@ -108,7 +111,7 @@ export const IndividualTask = ({
         </p>
       )}
 
-      {!highlight && (
+      {/* {!highlight && (
         <>
           {highlightCount === 0 && (
             <button
@@ -181,7 +184,131 @@ export const IndividualTask = ({
             }
           />
         </button>
-      )}
+      )} */}
+
+      <div className="w-56 text-right">
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex justify-center px-3 py-2 w-full bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+              <DotsVerticalIcon
+                className="w-5 h-5 text-white"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute inline-flex w-max flex-col items-center px-4 py-2 right-0 bg-black rounded-md bg-opacity-20 backdrop-blur hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 space-y-1 overflow-y-hidden">
+              {!highlight && (
+                <>
+                  <div>
+                    <Menu.Item>
+                      <div>
+                        {highlightCount === 0 && (
+                          <button
+                            title="Make Highlight"
+                            aria-label="Make Highlight"
+                            onClick={() => {
+                              onClick_makeHighlight({
+                                stateReload,
+                                todo_id
+                              });
+                            }}
+                          >
+                            ✨
+                          </button>
+                        )}
+                      </div>
+                    </Menu.Item>
+                  </div>
+                  <div>
+                    <Menu.Item>
+                      <button
+                        aria-label="Permanently Delete Task"
+                        title="Permanently Delete"
+                        onClick={() =>
+                          onClick_handleDelete({
+                            stateReload,
+                            todo_id
+                          })
+                        }
+                      >
+                        <TrashIcon className="w-6 h-6" />
+                      </button>
+                    </Menu.Item>
+                  </div>
+                  <div>
+                    <Menu.Item>
+                      <button
+                        title="Archive"
+                        aria-label="Archive Task"
+                        onClick={() => {
+                          set_todo_archive_state(!db_archive);
+                          onClick_toggleArchiving({
+                            stateReload,
+                            todo_archived: todo_archive_state,
+                            todo_id
+                          });
+                        }}
+                      >
+                        <ArchiveIcon className="w-6 h-6" />
+                      </button>
+                    </Menu.Item>
+                  </div>
+                </>
+              )}
+              <div>
+                <Menu.Item>
+                  <div>
+                    {storyid === todo_story_id ? (
+                      <button
+                        title="Remove from story"
+                        aria-label="Remove from story"
+                        className="flex items-center"
+                      >
+                        <EyeIcon
+                          className="w-6 h-6"
+                          onClick={() =>
+                            onClick_removeFromStory({
+                              stateReload,
+                              story_id: storyid,
+                              todo_id
+                            })
+                          }
+                        />
+                      </button>
+                    ) : (
+                      <button
+                        title="Add to story"
+                        aria-label="Add to story"
+                        className="flex items-center"
+                      >
+                        <EyeOffIcon
+                          className="w-6 h-6"
+                          onClick={() =>
+                            onClick_addToStory({
+                              stateReload,
+                              story_id: storyid,
+                              todo_id
+                            })
+                          }
+                        />
+                      </button>
+                    )}
+                  </div>
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
     </div>
   );
 };
