35f180ed024094b83d9c5010341abd108b1a7795
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-08-23 17:21:27 +0530

    Shifted to preact for prod

--- next.config.js
+++ next.config.js
@@ -31,6 +31,18 @@ module.exports = withPWA({
   //     },
   //   ]
   // },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
   reactStrictMode: true,
   images: {
     domains: ["avataaars.io"],
