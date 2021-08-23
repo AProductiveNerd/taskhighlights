8f4fb7eba152dc7a3155413b2119170143ff494e
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-08-23 17:08:11 +0530

    Revert "Shifted to using absolute URLs for API_V1"
    This reverts commit ef8c51f76f2a5b7b06652c3e984cdb92fa028f89.

--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -5,10 +5,5 @@ export const PROFILE = "/p/:username";
 export const COMPLETED = "/completed";
 export const ARCHIVED = "/archived";
 export const NOT_FOUND = "/404";
-export const API_V1 =
-  `${
-    process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000"
-  }` + "/api/v1/";
+export const API_V1 = "/api/v1/";
 export const NO_HEADER = ["/", "/sign-up", "/sign-in"];
