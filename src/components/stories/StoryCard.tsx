30080a80d602aeaf24c9e8f5473de2c2257fdd40
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-08-25 12:41:07 +0530

    Story removal is now reactive

--- src/components/stories/StoryCard.tsx
+++ src/components/stories/StoryCard.tsx
@@ -1,8 +1,8 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { Useful_Todo, todo_id } from "../../constants/Types";
+import { useEffect, useState } from "react";
 
 import { StoryTask } from "./StoryTask";
-import { useState } from "react";
 
 export const StoryCard = ({
   todos,
@@ -14,34 +14,36 @@ export const StoryCard = ({
   const [highlights, setHighlights] = useState(null);
   const [normalTodos, setNormalTodos] = useState(null);
 
-  setHighlights(todos.filter((todo) => todo.todo_highlight === true));
-  setNormalTodos(todos.filter((todo) => todo.todo_highlight === false));
+  useEffect(() => {
+    setHighlights(todos.filter((todo) => todo.todo_highlight === true));
+    setNormalTodos(todos.filter((todo) => todo.todo_highlight === false));
+  }, [todos]);
 
   const stateReload = (todo_id: todo_id): void => {
-    const newNormal = [...normalTodos];
-    const newHighlight = [...highlights];
+    const newHigh = [...highlights];
+    const newNorm = [...normalTodos];
 
-    for (let i = 0; i < newNormal.length; i++) {
-      if (newNormal[i].todo_id === todo_id) {
-        newNormal.splice(i, 1);
+    for (let i = 0; i < newHigh.length; i++) {
+      if (newHigh[i].todo_id === todo_id) {
+        newHigh.splice(i, 1);
       }
     }
 
-    setNormalTodos(newNormal);
-
-    for (let i = 0; i < newHighlight.length; i++) {
-      if (newHighlight[i].todo_id === todo_id) {
-        newHighlight.splice(i, 1);
+    for (let i = 0; i < newNorm.length; i++) {
+      if (newNorm[i].todo_id === todo_id) {
+        newNorm.splice(i, 1);
       }
     }
-    setHighlights(newHighlight);
+
+    setHighlights(newHigh);
+    setNormalTodos(newNorm);
   };
 
   return (
     <div className="noScrollbar relative space-y-5 max-h-[80vh] w-11/12 sm:max-w-md md:max-w-lg py-4 px-8 bg-theme-blueGray-800 shadow-lg rounded-lg mx-auto selection:bg-theme-primary-500/60 overflow-y-scroll overflow-x-hidden">
       <div className="space-y-2">
         {highlights &&
-          highlights.map((highlight) => (
+          highlights.map((highlight: Useful_Todo) => (
             <StoryTask
               key={highlight.todo_id}
               todo={highlight}
