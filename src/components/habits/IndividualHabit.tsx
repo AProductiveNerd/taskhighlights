bb9abca0cc279a629398d826f9a2ad5d8c53a2f5
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-09-23 01:20:35 +0530

    🎨Design Refactor: Used the IndividualItem component for IndividualHabit

--- src/components/habits/IndividualHabit.tsx
+++ src/components/habits/IndividualHabit.tsx
@@ -1,5 +1,6 @@
 import { Useful_Habit, todo_done } from "../../constants/Types";
 
+import { IndividualItem } from "../layout/IndividualItem";
 import { onClick_toggleHabitDone } from "../../utils/onClickHelpers";
 import { useState } from "react";
 
@@ -11,28 +12,29 @@ export const IndividualHabit = ({
   const [habit_state, setHabitState] = useState<todo_done>(db_done);
 
   return (
-    <div
-      className="flex items-center space-x-2 text-left text-lg break-words leading-6 group"
-      tabIndex={0}
-    >
-      <input
-        type="checkbox"
-        id={habit_id}
-        defaultChecked={habit_state}
-        onChange={() => {
-          setHabitState(!db_done);
-          onClick_toggleHabitDone({
-            habit_done: habit_state,
-            habit_id
-          });
-        }}
-      />
-
-      <p className={`${habit_state && "line-through"} w-full cursor-pointer`}>
-        <label className="cursor-pointer" htmlFor={habit_id}>
-          <p>{habit_description}</p>
-        </label>
-      </p>
-    </div>
+    <IndividualItem
+      input_checkbox={
+        <input
+          type="checkbox"
+          id={habit_id}
+          defaultChecked={habit_state}
+          onChange={() => {
+            setHabitState(!db_done);
+            onClick_toggleHabitDone({
+              habit_done: habit_state,
+              habit_id
+            });
+          }}
+        />
+      }
+      onkeydowncapture_callback={null}
+      text_input_p={
+        <p className={`${habit_state && "line-through"} w-full cursor-pointer`}>
+          <label className="cursor-pointer" htmlFor={habit_id}>
+            <p>{habit_description}</p>
+          </label>
+        </p>
+      }
+    />
   );
 };
--- src/components/layout/IndividualItem.tsx
+++ src/components/layout/IndividualItem.tsx
@@ -5,12 +5,12 @@ import { Fragment } from "react";
 interface IndividualMenu_Props {
   text_input_p: JSX.Element;
   input_checkbox: JSX.Element;
-  menu_items: JSX.Element | JSX.Element[];
+  menu_items?: JSX.Element | JSX.Element[];
   non_menu_button?: JSX.Element | JSX.Element[];
-  menu_show_button_icon: JSX.Element;
+  menu_show_button_icon?: JSX.Element;
   onkeydowncapture_callback: (
     event: React.KeyboardEvent<HTMLDivElement>
-  ) => void;
+  ) => void | null;
 }
 
 export const IndividualItem = ({
@@ -33,54 +33,56 @@ export const IndividualItem = ({
 
       {non_menu_button && <div>{non_menu_button}</div>}
 
-      <div>
-        <Menu
-          as="div"
-          className="relative inline-block text-left text-theme-blueGray-300"
-        >
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+      {menu_items && menu_show_button_icon && (
+        <div>
+          <Menu
+            as="div"
+            className="relative inline-block text-left text-theme-blueGray-300"
           >
-            <Menu.Items
-              className="
-              absolute right-0 w-56 mr-10 -mt-9 origin-bottom-left
-              bg-black bg-opacity-70 filter backdrop-blur-3xl
-              divide-gray-100 rounded-md
-              shadow-lg ring-1 ring-black ring-opacity-5
-              focus:outline-none
-              z-50
-            "
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
             >
-              <div className="px-1 py-1">{menu_items}</div>
-            </Menu.Items>
-          </Transition>
+              <Menu.Items
+                className="
+                  absolute right-0 w-56 mr-10 -mt-9 origin-bottom-left
+                  bg-black bg-opacity-70 filter backdrop-blur-3xl
+                  divide-gray-100 rounded-md
+                  shadow-lg ring-1 ring-black ring-opacity-5
+                  focus:outline-none
+                  z-50
+                "
+              >
+                <div className="px-1 py-1">{menu_items}</div>
+              </Menu.Items>
+            </Transition>
 
-          <div>
-            <Menu.Button
-              as="button"
-              title="Menu"
-              aria-label="Menu"
-              className="
-                inline-flex justify-center
-                w-full px-2 py-2
-                text-sm font-medium
-                rounded-md
-                bg-black bg-opacity-20 filter backdrop-blur-3xl
-                hover:bg-opacity-30 focus:outline-none focus-visible:ring-2
-                focus-visible:ring-white focus-visible:ring-opacity-75
-              "
-            >
-              {menu_show_button_icon}
-            </Menu.Button>
-          </div>
-        </Menu>
-      </div>
+            <div>
+              <Menu.Button
+                as="button"
+                title="Menu"
+                aria-label="Menu"
+                className="
+                  inline-flex justify-center
+                  w-full px-2 py-2
+                  text-sm font-medium
+                  rounded-md
+                  bg-black bg-opacity-20 filter backdrop-blur-3xl
+                  hover:bg-opacity-30 focus:outline-none focus-visible:ring-2
+                  focus-visible:ring-white focus-visible:ring-opacity-75
+                "
+              >
+                {menu_show_button_icon}
+              </Menu.Button>
+            </div>
+          </Menu>
+        </div>
+      )}
     </div>
   );
 };
