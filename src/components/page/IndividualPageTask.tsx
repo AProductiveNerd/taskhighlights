e3fa04b6ce747ac0dc0fb1529ff48db2740369a0
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-09-23 01:39:59 +0530

    🎨Design Refactor: Used the IndividualItem component for IndividualPageTask

--- src/components/page/IndividualPageTask.tsx
+++ src/components/page/IndividualPageTask.tsx
@@ -18,6 +18,8 @@ import {
   onClick_toggleTodoDone
 } from "../../utils/onClickHelpers";
 
+import { IndividualItem } from "../layout/IndividualItem";
+
 export const IndividualPageTask = ({
   todo: {
     todo_description,
@@ -44,138 +46,134 @@ export const IndividualPageTask = ({
   }, [display_text_edit]);
 
   return (
-    <div
-      className="flex items-center space-x-2 text-left text-lg break-words leading-6 group"
-      tabIndex={0}
-      onKeyDownCapture={(event) => {
+    <IndividualItem
+      input_checkbox={
+        <input
+          type="checkbox"
+          className="cursor-pointer"
+          id={todo_id}
+          defaultChecked={todo_state}
+          onChange={() => {
+            set_todo_state(!db_done);
+
+            onClick_toggleTodoDone({
+              todo_done: todo_state,
+              todo_id,
+              stateReload
+            });
+          }}
+        />
+      }
+      onkeydowncapture_callback={(event) => {
         if (event.key === "Delete") {
           onClick_handleDelete({ stateReload, todo_id });
         } else if (event.key === "Enter") {
           set_display_text_edit(true);
         }
       }}
-    >
-      <input
-        type="checkbox"
-        className="cursor-pointer"
-        id={todo_id}
-        defaultChecked={todo_state}
-        onChange={() => {
-          set_todo_state(!db_done);
-
-          onClick_toggleTodoDone({
-            todo_done: todo_state,
-            todo_id,
-            stateReload
-          });
-        }}
-      />
-
-      {display_text_edit === true ? (
-        <input
-          type="text"
-          className="w-full bg-theme-blueGray-800 cursor-pointer"
-          value={new_title}
-          onChange={({ target }) => set_new_title(target.value)}
-          ref={editTaskRef}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              onClick_handleTextSubmit({
-                todo_id,
-                todo_description: new_title,
-                stateReload,
-                set_display_text_edit
-              });
-              set_display_text_edit(false);
-            } else if (event.key === "Escape") {
-              set_display_text_edit(false);
-              set_new_title(todo_description);
-            }
-          }}
-        />
-      ) : (
-        <p
-          className={`${todo_state && "line-through"} w-full cursor-pointer`}
-          onClick={() => set_display_text_edit(!display_text_edit)}
-        >
-          <label className="cursor-pointer" htmlFor={todo_id}>
-            <p>{new_title}</p>
-          </label>
-        </p>
-      )}
-
-      <div className="text-right flex-shrink">
-        <Menu as="div" className="inline-block text-left">
-          <div>
-            <Menu.Button
-              title="Menu"
-              aria-label="Menu"
-              as="button"
-              className="
-                inline-flex justify-center
-                w-full px-2 py-2
-                text-sm font-medium
-                rounded-md
-                bg-black bg-opacity-30 filter backdrop-blur-3xl
-                hover:bg-opacity-40 focus:outline-none focus-visible:ring-2
-                focus-visible:ring-white focus-visible:ring-opacity-75
-              "
+      text_input_p={
+        <>
+          {display_text_edit === true ? (
+            <input
+              type="text"
+              className="w-full bg-theme-blueGray-800 cursor-pointer"
+              value={new_title}
+              onChange={({ target }) => set_new_title(target.value)}
+              ref={editTaskRef}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  onClick_handleTextSubmit({
+                    todo_id,
+                    todo_description: new_title,
+                    stateReload,
+                    set_display_text_edit
+                  });
+                  set_display_text_edit(false);
+                } else if (event.key === "Escape") {
+                  set_display_text_edit(false);
+                  set_new_title(todo_description);
+                }
+              }}
+            />
+          ) : (
+            <p
+              className={`${
+                todo_state && "line-through"
+              } w-full cursor-pointer`}
+              onClick={() => set_display_text_edit(!display_text_edit)}
             >
-              <DotsVerticalIcon
-                className="w-4 h-4 text-theme-primary-50"
-                aria-hidden="true"
-              />
-            </Menu.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="sticky inline-flex w-max flex-col items-center px-2.5 py-2 right-0 bg-black rounded-md bg-opacity-20 backdrop-blur hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 space-y-1 overflow-y-hidden mt-1">
-              <div>
-                <Menu.Item>
-                  <button
-                    aria-label="Permanently Delete Task"
-                    title="Permanently Delete"
-                    onClick={() =>
-                      onClick_handleDelete({
-                        stateReload,
-                        todo_id
-                      })
-                    }
-                  >
-                    <TrashIcon className="w-6 h-6" />
-                  </button>
-                </Menu.Item>
-              </div>
+              <label className="cursor-pointer" htmlFor={todo_id}>
+                <p>{new_title}</p>
+              </label>
+            </p>
+          )}
+        </>
+      }
+      menu_items={
+        <div className="px-1 py-1">
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                aria-label="Permanently Delete Task"
+                title="Permanently Delete"
+                onClick={() =>
+                  onClick_handleDelete({
+                    stateReload,
+                    todo_id
+                  })
+                }
+                className={`${
+                  active
+                    ? "bg-theme-primary-500 text-theme-blueGray-300"
+                    : "text-theme-blueGray-500"
+                } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+              >
+                <TrashIcon
+                  className={`w-6 h-6 ${
+                    !active && "text-theme-primary-500"
+                  } mr-2`}
+                />
+                Permanently Delete
+              </button>
+            )}
+          </Menu.Item>
 
-              <div>
-                <Menu.Item>
-                  <button
-                    title="Archive"
-                    aria-label="Archive Task"
-                    onClick={() => {
-                      set_todo_archive_state(!db_archive);
-                      onClick_toggleArchiving({
-                        stateReload,
-                        todo_archived: todo_archive_state,
-                        todo_id
-                      });
-                    }}
-                  >
-                    <ArchiveIcon className="w-6 h-6" />
-                  </button>
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
-      </div>
-    </div>
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                title="Archive Task"
+                aria-label="Archive Task"
+                onClick={() => {
+                  set_todo_archive_state(!db_archive);
+                  onClick_toggleArchiving({
+                    stateReload,
+                    todo_archived: todo_archive_state,
+                    todo_id
+                  });
+                }}
+                className={`${
+                  active
+                    ? "bg-theme-primary-500 text-theme-blueGray-300"
+                    : "text-theme-blueGray-500"
+                } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+              >
+                <ArchiveIcon
+                  className={`w-6 h-6 ${
+                    !active && "text-theme-primary-500"
+                  } mr-2`}
+                />
+                Archive Task
+              </button>
+            )}
+          </Menu.Item>
+        </div>
+      }
+      menu_show_button_icon={
+        <DotsVerticalIcon
+          className="w-5 h-5 text-theme-primary-50"
+          aria-hidden="true"
+        />
+      }
+    />
   );
 };
