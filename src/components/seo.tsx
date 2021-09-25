8dca3962a76524eb241564d6a573f51db8773546
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-09-25 14:28:03 +0530

    🚀New Feature: Twitter Cardz!

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,6 +1,20 @@
+import Head from "next/head";
 import { NextSeo } from "next-seo";
 import { SEO_interface } from "../constants/Types";
 
 export const SEO_component = (seo_props: SEO_interface): JSX.Element => {
-  return <NextSeo {...seo_props} />;
+  return (
+    <>
+      <Head>
+        <meta name="twitter:title" content={seo_props.title} />
+        <meta name="twitter:description" content={seo_props.description} />
+        <meta name="twitter:site" content="@taskhighlights" />
+        {seo_props.twitter.handle && (
+          <meta name="twitter:author" content={seo_props.twitter.handle} />
+        )}
+        <meta name="twitter:image" content="/icon.png" />
+      </Head>
+      <NextSeo {...seo_props} />
+    </>
+  );
 };
