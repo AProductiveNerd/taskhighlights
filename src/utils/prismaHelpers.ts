ecc8e5253f00bb9a687aaae70446d6732ad8d43d
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-10-06 22:27:40 +0530

    🧑🏼‍💻Dev: Changing PrismaClient instantiation in development

--- src/utils/prismaHelpers.ts
+++ src/utils/prismaHelpers.ts
@@ -11,7 +11,11 @@ import {
   User
 } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// Prevent multiple instances of Prisma Client in development
+declare const global: NodeJS.Global & { prisma?: PrismaClient };
+
+export const prisma = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export const prisma_getUserByUsername = async (
   user_username: TYPES.user_username,
