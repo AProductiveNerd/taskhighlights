a17a52cd5e02211110e49ccc1200425eb0e6a627
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-10-14 00:20:07 +0530

    🧑🏼‍💻Dev: Using absolute URL png for both OG and Twitter

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -12,7 +12,7 @@ export const default_seo = (pathname: string): NextSeoProps => {
         "A to-do app that uses accountability to supercharge your productivity",
       images: [
         {
-          url: "/icon.png",
+          url: "https://www.taskhighlights.com/icon.png",
           alt: "Task Highlights"
         }
       ],
